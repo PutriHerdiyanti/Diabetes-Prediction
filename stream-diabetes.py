687965429514b9eb3e6ddabff137a84ab90c078d
PutriHerdiyanti <putriherdiyanti15@gmail.com>
2025-10-02 22:22:36 +0700

    Update stream-diabetes.py

--- stream-diabetes.py
+++ stream-diabetes.py
@@ -2,7 +2,7 @@ import pickle
 import streamlit as st
 
 # membaca model
-diabetes_model = pickle.load(open('diabetes_model.sav'))
+diabetes_model = pickle.load(open(r'D:\Latihan_Data Mining\diabetes_model.sav', 'rb'))
 
 #judul web
 st.title('Data Mining Prediksi Diabetes')
@@ -22,3 +22,17 @@ BMI = st.text_input ('input nilai BMI')
 DiabetesPredigreeFunction = st.text_input ('input nilai Diabetes Predigree Function')
 
 Age = st.text_input ('input nilai Age')
+
+# code untuk prediksi
+dilab_diagnosis = ''
+
+# membuat tombol untuk prediksi
+if st.button('Test Prediksi Diabetes') :
+    diab_prediction = diabetes_model.predict([[Pregnancies, Glucose, BloodPressure, SkinThickness, Insulin, BMI, DiabetesPredigreeFunction, Age]])
+
+    if(diab_prediction[0] == 1):
+        diab_diagnosis = 'Pasien terkena Diabetes'
+    else :
+        diab_diagnosis = 'Pasien tidak terkena Diabetes'
+
+    st.sucesss(dilab_diagnosis)
